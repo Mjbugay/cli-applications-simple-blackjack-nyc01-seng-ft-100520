80d048e19de3ab68a806b5e0bd9b92b57ca1dfd6
Matt Bugay <mjbugay@yahoo.com>
2020-09-27 19:58:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,9 +23,10 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card
-  deal_card
-  display_card_total(deal_card + deal_card)
+  card1 = deal_card
+  card2 = deal_card
+  sum = card1 + card2
+  
 end
 
 def hit?
